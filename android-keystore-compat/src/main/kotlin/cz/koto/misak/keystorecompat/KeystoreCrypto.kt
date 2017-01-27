fe3b713922116f634f70e57c60ed2b8b3387e662
Michal Jenicek <michal.jenicek@strv.com>
2017-01-27 13:30:27 +0100

    Add local aar from the Bintray (as the Jcenter distribution is waiting too much now).

--- android-keystore-compat/src/main/kotlin/cz/koto/misak/keystorecompat/KeystoreCrypto.kt
+++ android-keystore-compat/src/main/kotlin/cz/koto/misak/keystorecompat/KeystoreCrypto.kt
@@ -4,16 +4,109 @@ import android.util.Base64
 import android.util.Log
 import java.io.ByteArrayInputStream
 import java.io.ByteArrayOutputStream
-import java.security.KeyStore
+import java.nio.ByteBuffer
+import java.nio.ByteOrder
+import java.security.*
 import java.security.interfaces.RSAPublicKey
 import java.util.*
-import javax.crypto.Cipher
-import javax.crypto.CipherInputStream
-import javax.crypto.CipherOutputStream
+import javax.crypto.*
+import javax.crypto.spec.IvParameterSpec
 
 internal object KeystoreCrypto {
 
     private val LOG_TAG = javaClass.name
+    val ORDER_FOR_ENCRYPTED_DATA = ByteOrder.BIG_ENDIAN
+
+    fun encryptKey(privateKeyEntry: KeyStore.PrivateKeyEntry, key: ByteArray): ByteArray {
+        var iv: ByteArray
+        var encryptedKeyForRealm: ByteArray
+        try {
+            val publicKey = privateKeyEntry.certificate.publicKey as RSAPublicKey
+
+            /**
+             * AndroidOpenSSL works on Lollipop.
+             * But on marshmallow it throws: java.security.InvalidKeyException: Need RSA private or public key
+             *
+             * On Android 6.0 you should Not use "AndroidOpenSSL" for cipher creation,
+             * it would fail with "Need RSA private or public key" at cipher init for decryption.
+             * Simply use Cipher.getInstance("RSA/ECB/PKCS1Padding")
+             */
+            val inCipher = Cipher.getInstance(KeystoreCompat.cipherMode/*, "AndroidOpenSSL"*/)
+            inCipher.init(Cipher.ENCRYPT_MODE, publicKey)
+
+            encryptedKeyForRealm = inCipher.doFinal(key)
+            iv = inCipher.iv
+
+
+        } catch (e: Exception) {
+            Log.e(LOG_TAG, "Encryption2 error", e)
+            throw e
+        }
+        val ivAndEncryptedKey = ByteArray(Integer.SIZE + iv.size + encryptedKeyForRealm.size)
+
+        val buffer = ByteBuffer.wrap(ivAndEncryptedKey)
+        buffer.order(ORDER_FOR_ENCRYPTED_DATA)
+        buffer.putInt(iv.size)
+        buffer.put(iv)
+        buffer.put(encryptedKeyForRealm)
+
+        //val outputStream = ByteArrayOutputStream()
+        //val cipherOutputStream = CipherOutputStream(outputStream, inCipher)
+        //cipherOutputStream.write(encryptedKeyForRealm)
+        //cipherOutputStream.close()
+
+        return ivAndEncryptedKey
+    }
+
+    fun decryptKey(privateKeyEntry: KeyStore.PrivateKeyEntry, ivAndEncryptedKey: ByteArray): ByteArray {
+
+        val buffer = ByteBuffer.wrap(ivAndEncryptedKey)
+        buffer.order(ORDER_FOR_ENCRYPTED_DATA)
+
+        val ivLength = buffer.int
+        val iv = ByteArray(ivLength)
+        val encryptedKey = ByteArray(ivAndEncryptedKey.size - Integer.SIZE - ivLength)
+
+        buffer.get(iv)
+        buffer.get(encryptedKey)
+
+        try {
+            /**
+             * AndroidOpenSSL works on Lollipop.
+             * But on marshmallow it throws: java.security.InvalidKeyException: Need RSA private or public key
+             *
+             * On Android 6.0 you should Not use "AndroidOpenSSL" for cipher creation,
+             * it would fail with "Need RSA private or public key" at cipher init for decryption.
+             * Simply use Cipher.getInstance("RSA/ECB/PKCS1Padding")
+             */
+            val cipher = Cipher.getInstance(KeystoreCompat.cipherMode/*, "AndroidOpenSSL"*/)
+            val ivSpec = IvParameterSpec(iv)
+            cipher.init(Cipher.DECRYPT_MODE, privateKeyEntry.privateKey, ivSpec)
+
+            return cipher.doFinal(encryptedKey)
+
+        } catch (e: Exception) {
+            when (e) {
+                is InvalidKeyException -> {
+                    throw RuntimeException("key is invalid.")
+                }
+                is UnrecoverableKeyException -> {
+                }
+                is NoSuchAlgorithmException -> {
+                }
+                is BadPaddingException -> {
+                }
+                is KeyStoreException -> {
+                }
+                is IllegalBlockSizeException -> {
+                }
+                is InvalidAlgorithmParameterException -> {
+                }
+            }
+            throw e
+        }
+    }
+
 
     fun encryptCredentials(composedCredentials: String, privateKeyEntry: KeyStore.PrivateKeyEntry): String {
         try {
