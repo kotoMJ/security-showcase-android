81b31fea887f2254e8b478a3b0586cb6e0447c14
kotomisak <jenicek.michal@gmail.com>
2017-09-18 12:31:24 +0200

    First steps to use arch components instead of custom databinding library.

--- app/src/main/kotlin/cz/koto/securityshowcase/ui/login/LoginActivity.kt
+++ app/src/main/kotlin/cz/koto/securityshowcase/ui/login/LoginActivity.kt
@@ -1,33 +1,85 @@
 package cz.koto.securityshowcase.ui.login
 
 import android.app.Activity
+import android.arch.lifecycle.ViewModelProviders
 import android.content.Intent
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import cz.koto.keystorecompat.KeystoreCompat
+import cz.koto.keystorecompat.exception.ForceLockScreenKitKatException
+import cz.koto.keystorecompat.utility.forceAndroidAuth
+import cz.koto.keystorecompat.utility.runSinceKitKat
 import cz.koto.securityshowcase.R
 import cz.koto.securityshowcase.databinding.ActivityLoginBinding
-import cz.koto.securityshowcase.ui.BaseActivity
+import cz.koto.securityshowcase.storage.CredentialStorage
+import cz.koto.securityshowcase.utility.Logcat
 
-class LoginActivity : BaseActivity<ActivityLoginBinding, LoginViewModel>() {
+class LoginActivity : AppCompatActivity() {
+
 
 	companion object {
 		val FORCE_SIGNUP_REQUEST = 1111
 	}
 
+	private lateinit var viewModel: LoginViewModel
+	private lateinit var viewDataBinding: ActivityLoginBinding
+
+
+
 	override fun onCreate(savedInstanceState: Bundle?) {
-		setupViewModel(R.layout.activity_login, LoginViewModel::class.java)
 		super.onCreate(savedInstanceState)
+		setContentView(R.layout.activity_login)
+
+		viewModel = ViewModelProviders.of(this).get(LoginViewModel::class.java)
+//		viewDataBinding = DataBindingUtil.inflate<ActivityLoginBinding>(layoutInflater, R.layout.activity_login, null, false).apply{
+//			//Use this in fragment or case where you don't request viewModel directly.
+//			//viewmodel = (activity as TasksActivity).obtainViewModel()
+//		}
+		viewDataBinding = ActivityLoginBinding.inflate(layoutInflater)
+
+		onLoginDisplayed(true)
+		viewDataBinding.executePendingBindings()
 	}
 
 	override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
 		if (requestCode == FORCE_SIGNUP_REQUEST) {
 			if (resultCode == Activity.RESULT_CANCELED) {
 				KeystoreCompat.increaseLockScreenCancel()
-				activity.finish()
+				this.finish()
 			} else {
-				viewModel.onViewAttached(false)
+				onLoginDisplayed(false)
 			}
 		} else
 			super.onActivityResult(requestCode, resultCode, data)
+	}
+
+	fun onLoginDisplayed(firstAttachment: Boolean) {
+		runSinceKitKat {
+			if (KeystoreCompat.hasSecretLoadable()) {
+				KeystoreCompat.loadSecretAsString({ decryptResult ->
+					decryptResult.split(';').let {
+						viewModel?.email?.set(it[0])
+						viewModel?.password?.set(it[1])
+						viewModel?.signInGql()
+//						viewDataBinding.viewModel?.email?.set(it[0])
+//						viewDataBinding.viewModel?.password?.set(it[1])
+//						viewDataBinding.viewModel?.signInGql()
+					}
+				}, { exception ->
+					CredentialStorage.dismissForceLockScreenFlag()
+					if (exception is ForceLockScreenKitKatException) {
+						this.startActivityForResult(exception.lockIntent, FORCE_SIGNUP_REQUEST)
+					} else {
+						Logcat.e(exception, "")
+						CredentialStorage.performLogout()
+						forceAndroidAuth(getString(R.string.kc_lock_screen_title), getString(R.string.kc_lock_screen_description),
+								{ intent -> this.startActivityForResult(intent, FORCE_SIGNUP_REQUEST) },
+								KeystoreCompat.context)
+					}
+				}, CredentialStorage.forceLockScreenFlag)
+			} else {
+				Logcat.d("Use standard login.")
+			}
+		}
 	}
 }
--- app/src/main/kotlin/cz/koto/securityshowcase/ui/login/LoginViewModel.kt
+++ app/src/main/kotlin/cz/koto/securityshowcase/ui/login/LoginViewModel.kt
@@ -1,37 +1,28 @@
 package cz.koto.securityshowcase.ui.login
 
+//import cz.koto.securityshowcase.ContextProvider
+import android.app.Application
+import android.arch.lifecycle.AndroidViewModel
 import android.content.Intent
 import android.databinding.Observable
 import android.databinding.ObservableBoolean
 import android.databinding.ObservableField
 import android.net.Uri
+import android.support.v4.app.ActivityCompat
 import com.apollographql.android.rx2.Rx2Apollo
-import cz.koto.keystorecompat.KeystoreCompat
-import cz.koto.keystorecompat.exception.ForceLockScreenKitKatException
-import cz.koto.keystorecompat.utility.forceAndroidAuth
-import cz.koto.keystorecompat.utility.runSinceKitKat
-import cz.koto.securityshowcase.ContextProvider
 import cz.koto.securityshowcase.Login
-import cz.koto.securityshowcase.R
 import cz.koto.securityshowcase.SecurityConfig
 import cz.koto.securityshowcase.api.SecurityShowcaseApiProvider
-import cz.koto.securityshowcase.databinding.ActivityLoginBinding
 import cz.koto.securityshowcase.model.AuthRequestSimple
 import cz.koto.securityshowcase.storage.CredentialStorage
-import cz.koto.securityshowcase.ui.BaseViewModel
 import cz.koto.securityshowcase.ui.StateListener
-import cz.koto.securityshowcase.ui.login.LoginActivity.Companion.FORCE_SIGNUP_REQUEST
-import cz.koto.securityshowcase.ui.main.MainActivity
-import cz.koto.securityshowcase.utility.Logcat
 import cz.koto.securityshowcase.utility.isValidJWT
 import cz.koto.securityshowcase.utility.longPref
-import cz.koto.securityshowcase.utility.start
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
 import org.alfonz.view.StatefulLayout
 
-class LoginViewModel : BaseViewModel<ActivityLoginBinding>(), StateListener {
-
+class LoginViewModel(val context: Application) : /*BaseViewModel<ActivityLoginBinding>()*/AndroidViewModel(context), StateListener {
 
 	val devAvailable = ObservableBoolean(SecurityConfig.isEndpointDev() && !SecurityConfig.isPackageRelease())
 
@@ -53,44 +44,16 @@ class LoginViewModel : BaseViewModel<ActivityLoginBinding>(), StateListener {
 	val state = ObservableField(StatefulLayout.CONTENT)
 
 
-	override fun onViewModelCreated() {
-		super.onViewModelCreated()
+	init {
 		CredentialStorage.forceLockScreenFlag()
 		email.addOnPropertyChangedCallback(userNameChanged)
 	}
 
-	override fun onViewAttached(firstAttachment: Boolean) {
-		super.onViewAttached(firstAttachment)
-		runSinceKitKat {
-			if (KeystoreCompat.hasSecretLoadable()) {
-				KeystoreCompat.loadSecretAsString({ decryptResult ->
-					decryptResult.split(';').let {
-						email.set(it[0])
-						password.set(it[1])
-						signInGql()
-					}
-				}, { exception ->
-					CredentialStorage.dismissForceLockScreenFlag()
-					if (exception is ForceLockScreenKitKatException) {
-						activity.startActivityForResult(exception.lockIntent, FORCE_SIGNUP_REQUEST)
-					} else {
-						Logcat.e(exception, "")
-						CredentialStorage.performLogout()
-						forceAndroidAuth(getString(R.string.kc_lock_screen_title), getString(R.string.kc_lock_screen_description),
-								{ intent -> activity.startActivityForResult(intent, FORCE_SIGNUP_REQUEST) },
-								KeystoreCompat.context)
-					}
-				}, CredentialStorage.forceLockScreenFlag)
-			} else {
-				Logcat.d("Use standard login.")
-			}
-		}
-	}
-
-	override fun onViewModelDestroyed() {
-		super.onViewModelDestroyed()
+	override fun onCleared() {
+		super.onCleared()
 		email.removeOnPropertyChangedCallback(userNameChanged)
 	}
+
 
 	override fun setProgress() {
 		state.set(StatefulLayout.PROGRESS)
@@ -144,13 +107,14 @@ class LoginViewModel : BaseViewModel<ActivityLoginBinding>(), StateListener {
 
 	private fun showMain() {
 		stringChallenge = ""
-		activity.finish()
-		activity.start<MainActivity>()
+		//TODO
+//		activity.finish()
+//		activity.start<MainActivity>()
 	}
 
 	fun followGithub() {
 		val intent = Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/kotomisak/security-showcase-android"))
 		intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-		ContextProvider.context.startActivity(intent)
+		ActivityCompat.startActivity(context, intent, null)
 	}
 }
