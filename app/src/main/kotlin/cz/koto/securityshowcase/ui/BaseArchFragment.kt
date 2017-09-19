e5ca5c489b09c2764dcc044da3aff3770ffaf351
kotomisak <jenicek.michal@gmail.com>
2017-09-19 08:26:14 +0200

    Migrate InfoFragment/InfoViewModel to architecture components.

---
+++ app/src/main/kotlin/cz/koto/securityshowcase/ui/BaseArchFragment.kt
@@ -0,0 +1,7 @@
+package cz.koto.securityshowcase.ui
+
+import android.support.v4.app.Fragment
+
+open class BaseArchFragment : Fragment() {
+
+}
--- app/src/main/kotlin/cz/koto/securityshowcase/ui/info/InfoViewModel.kt
+++ app/src/main/kotlin/cz/koto/securityshowcase/ui/info/InfoViewModel.kt
@@ -1,42 +1,25 @@
 package cz.koto.securityshowcase.ui.info
 
-import cz.koto.securityshowcase.databinding.FragmentInfoBinding
-import cz.koto.securityshowcase.ui.BaseViewModel
+import android.arch.lifecycle.ViewModel
 
-class InfoViewModel : BaseViewModel<FragmentInfoBinding>() {
+class InfoViewModel : ViewModel() {
 
-//	val state = ObservableField(StatefulLayout.State.CONTENT)
-
-
-	override fun onViewModelCreated() {
-		super.onViewModelCreated()
-	}
-
-
-	override fun onResume() {
-		super.onResume()
-		//TODO fix this using arch
-//		activity.showToolbar(true)
-//		activity.getSupportActionBar()?.setIcon(R.drawable.ic_security_24dp)
-//		activity.getSupportActionBar()?.setTitle("")
-	}
-
-
-	override fun onViewAttached(firstAttachment: Boolean) {
-		super.onViewAttached(firstAttachment)
-		//TODO fix this using arch
-//		activity.getBinding().executePendingBindings()
-//		activity.getSupportActionBar()?.setIcon(R.drawable.ic_security_24dp)
-//		activity.getSupportActionBar()?.setTitle("")
-	}
-
-
-	override fun onViewDetached(finalDetachment: Boolean) {
-		super.onViewDetached(finalDetachment)
-	}
-
-	override fun onViewModelDestroyed() {
-		super.onViewModelDestroyed()
-	}
+//	override fun onResume() {
+//		super.onResume()
+//		//TODO fix this using arch
+////		activity.showToolbar(true)
+////		activity.getSupportActionBar()?.setIcon(R.drawable.ic_security_24dp)
+////		activity.getSupportActionBar()?.setTitle("")
+//	}
+//
+//
+//	override fun onViewAttached(firstAttachment: Boolean) {
+//		super.onViewAttached(firstAttachment)
+//		//TODO fix this using arch
+////		activity.getBinding().executePendingBindings()
+////		activity.getSupportActionBar()?.setIcon(R.drawable.ic_security_24dp)
+////		activity.getSupportActionBar()?.setTitle("")
+//	}
+//
 
 }
