ab10f2d875f8d7d449d5bf84e0d1f15e77f2ef04
Michal <jenicek.michal@gmail.com>
2019-04-10 12:14:41 +0200

    Update sample app to be using Keystore Compat 2.0.3.

--- app/src/main/kotlin/cz/koto/securityshowcase/ui/info/InfoViewModel.kt
+++ app/src/main/kotlin/cz/koto/securityshowcase/ui/info/InfoViewModel.kt
@@ -1,25 +1,10 @@
 package cz.koto.securityshowcase.ui.info
 
 import android.arch.lifecycle.ViewModel
+import cz.koto.securityshowcase.BuildConfig
 
 class InfoViewModel : ViewModel() {
 
-//	override fun onResume() {
-//		super.onResume()
-//		//TODO fix this using arch
-////		activity.showToolbar(true)
-////		activity.getSupportActionBar()?.setIcon(R.drawable.ic_security_24dp)
-////		activity.getSupportActionBar()?.setTitle("")
-//	}
-//
-//
-//	override fun onViewAttached(firstAttachment: Boolean) {
-//		super.onViewAttached(firstAttachment)
-//		//TODO fix this using arch
-////		activity.getBinding().executePendingBindings()
-////		activity.getSupportActionBar()?.setIcon(R.drawable.ic_security_24dp)
-////		activity.getSupportActionBar()?.setTitle("")
-//	}
-//
+	fun getVersion() = "${BuildConfig.VERSION_CODE} - ${BuildConfig.VERSION_NAME}"
 
 }
