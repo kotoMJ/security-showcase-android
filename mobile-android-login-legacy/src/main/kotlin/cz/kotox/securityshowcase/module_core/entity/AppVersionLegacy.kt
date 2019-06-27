c1ab3a297f0493e6bb83c0a84ffbbaf56cfcbab5
kotomisak <jenicek.michal@gmail.com>
2019-06-27 21:35:41 +0200

    Remove legacy login application, since it is already in V2 at disposal.

--- mobile-android-login-legacy/src/main/kotlin/cz/kotox/securityshowcase/module_core/entity/AppVersionLegacy.kt
+++
@@ -1,3 +0,0 @@
-package cz.kotox.securityshowcase.module_core.entity
-
-data class AppVersionLegacy(val versionCode: Int, val versionName: String)
--- mobile-android-login-legacy/src/main/kotlin/cz/kotox/securityshowcase/module_login/LoginViewModelLegacy.kt
+++
@@ -1,6 +0,0 @@
-package cz.kotox.securityshowcase.module_login
-
-import cz.kotox.securityshowcase.module_core.arch.BaseViewModelLegacy
-import javax.inject.Inject
-
-class LoginViewModelLegacy @Inject constructor() : BaseViewModelLegacy()
